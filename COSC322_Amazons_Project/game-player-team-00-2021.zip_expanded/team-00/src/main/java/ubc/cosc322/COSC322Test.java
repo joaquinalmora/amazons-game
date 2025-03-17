8f797e736f9f412b9079701e96e953f9ced1172b
CaiLewendon <cailewendon@hotmail.com>
2025-03-17 15:10:11 -0700

    added option to join other rooms, stopped game from crashing midway through game. plays full game

--- COSC322_Amazons_Project/game-player-team-00-2021.zip_expanded/team-00/src/main/java/ubc/cosc322/COSC322Test.java
+++ COSC322_Amazons_Project/game-player-team-00-2021.zip_expanded/team-00/src/main/java/ubc/cosc322/COSC322Test.java
@@ -14,23 +14,23 @@ import ygraph.ai.smartfox.games.amazons.HumanPlayer;
 
 public class COSC322Test extends GamePlayer {
 
-    // Our connection objects: the game client and GUI
+    //our connection objects: the game client and GUI
     private GameClient client;
     private BaseGameGUI gui;
 
-    // credentials (username and a placeholder password)
+    // username and a placeholder password
     private String username;
     private String password;
 
-    // Game engine variables: Monte Carlo search instance and identifiers for pieces.
+    // Game engine variables: Monte Carlo search instance and identifiers for pieces
     private MonteCarlo monteCarlo;
     private final int WHITE_QUEEN = 1;
     private final int BLACK_QUEEN = 2;
-    private final int ARROW = 3; // Just a distinct value from the queens.
+    private final int ARROW = 3; // Just a distinct value from the queens
     private int myQueen = -1;
     private int opponentQueen = -1;
 
-    // start of the application. provide a username as an argument.
+    // provide a username as an argument
     public static void main(String[] args) {
         String uname = "cosc322";
         if (args.length > 0) {
@@ -51,7 +51,7 @@ public class COSC322Test extends GamePlayer {
         }
     }
 
-    //  sets up user credentials and instantiates the GUI.
+    //  sets up user credentials and instantiates the GUI
     public COSC322Test(String username, String password) {
         this.username = username;
         this.password = password;
@@ -78,7 +78,7 @@ public class COSC322Test extends GamePlayer {
         return gui;
     }
 
-    // Called once login is successful. It updates the GUI with room info and auto-joins the first room
+    //Called once login is successful. It updates the GUI with room info and auto-joins the first room
     @Override
     public void onLogin() {
         username = client.getUserName();
@@ -95,6 +95,12 @@ public class COSC322Test extends GamePlayer {
         } else {
             System.out.println("Couldn't find any room to join!");
         }
+    }
+
+    // New method: allows the user to manually join a different room from the GUI
+    public void joinRoom(String roomName) {
+        System.out.println("Joining room: " + roomName);
+        client.joinRoom(roomName);
     }
 
     // Responds to game messages coming from the server
@@ -122,27 +128,26 @@ public class COSC322Test extends GamePlayer {
             case GameMessage.GAME_ACTION_START:
                 String whitePlayer = (String) msgDetails.get(AmazonsGameMessage.PLAYER_WHITE);
                 String blackPlayer = (String) msgDetails.get(AmazonsGameMessage.PLAYER_BLACK);
-                assignQueens(whitePlayer, blackPlayer);
-                setupBoard();
+                SetMyQueen(whitePlayer, blackPlayer);
+                InitalizeBoard();
                 if (myQueen == BLACK_QUEEN) {
                     executeMove();
                 }
                 break;
-
             case "cosc322.game-state.userlost":
-                System.out.println("Game over, you lost.");
+                // Handle the user lost message
+                System.out.println("Received game-state.userlost. Game over.");
                 break;
 
             default:
-                System.out.println("Unexpected message type: " + messageType);
-                assert false;
+                System.out.println("Unhandled message type: " + messageType);
+                assert (false);
                 break;
         }
         return true;
     }
 
-    //figuring out which color we're playing with by comparing our username with the given names
-    private void assignQueens(String whitePlayer, String blackPlayer) {
+    public void SetMyQueen(String whitePlayer, String blackPlayer) {
         assert (!whitePlayer.equals(blackPlayer));
         if (username.equals(whitePlayer)) {
             myQueen = WHITE_QUEEN;
@@ -151,67 +156,71 @@ public class COSC322Test extends GamePlayer {
             myQueen = BLACK_QUEEN;
             opponentQueen = WHITE_QUEEN;
         } else {
-            System.out.println("Error: unknown queen assignment (" + myQueen + "). Aborting...");
-            assert false;
+            System.out.println("Fatal error, invalid queen value received " + myQueen + ", please restart");
+            assert (false);
         }
-        System.out.println("Our queen color is: " + myQueen);
+
+        System.out.println("SetMyQueen " + myQueen);
     }
 
-    // Uses the Monte Carlo Tree Search to decide on a move and then performs it
-    private void executeMove() {
+    public void executeMove() {
+        // assert that monte carlo is tracking turns properly as we can only make moves on our turn
         assert (monteCarlo.root.getColor() == myQueen);
+
         AmazonsAction action = monteCarlo.MCTS();
         if (action != null) {
-            ArrayList<Integer> posCurrent = new ArrayList<>();
-            posCurrent.add(action.queenSrcY + 1);
-            posCurrent.add(action.queenSrcX + 1);
+            ArrayList<Integer> aiQueenPosCurr = new ArrayList<Integer>();
+            aiQueenPosCurr.add(action.queenSrcY + 1);
+            aiQueenPosCurr.add(action.queenSrcX + 1);
 
-            ArrayList<Integer> posNext = new ArrayList<>();
-            posNext.add(action.queenDestY + 1);
-            posNext.add(action.queenDestX + 1);
+            ArrayList<Integer> aiQueenPosNext = new ArrayList<Integer>();
+            aiQueenPosNext.add(action.queenDestY + 1);
+            aiQueenPosNext.add(action.queenDestX + 1);
 
-            ArrayList<Integer> posArrow = new ArrayList<>();
-            posArrow.add(action.arrowDestY + 1);
-            posArrow.add(action.arrowDestX + 1);
+            ArrayList<Integer> aiArrowPos = new ArrayList<Integer>();
+            aiArrowPos.add(action.arrowDestY + 1);
+            aiArrowPos.add(action.arrowDestX + 1);
 
-            gui.updateGameState(posCurrent, posNext, posArrow);
-            client.sendMoveMessage(posCurrent, posNext, posArrow);
+            gui.updateGameState(aiQueenPosCurr, aiQueenPosNext, aiArrowPos);
+            client.sendMoveMessage(aiQueenPosCurr, aiQueenPosNext, aiArrowPos);
             monteCarlo.rootFromAction(action);
-        } else {
-            System.out.println("No moves left – seems like it's game over.");
-            // Her you might want to notify the server about the game ending
+        } else { // action is only null when you lose, as you have no actions available
+            System.out.println("You lose");
+            // TODO: is there a server message you send once the game is over?
         }
     }
 
-    // Applies the move that the opponent made. updates the visual board and the AIs internal state
-    private void processOpponentMove(ArrayList<Integer> currQueenPos, ArrayList<Integer> nextQueenPos,
-                                     ArrayList<Integer> arrowPos) {
+    public void processOpponentMove(ArrayList<Integer> currQueenPos, ArrayList<Integer> nextQueenPos,
+                                    ArrayList<Integer> arrowPos) {
         AmazonsAction action = new AmazonsAction(
-                currQueenPos.get(1) - 1,
-                currQueenPos.get(0) - 1,
-                nextQueenPos.get(1) - 1,
-                nextQueenPos.get(0) - 1,
-                arrowPos.get(1) - 1,
-                arrowPos.get(0) - 1);
+            currQueenPos.get(1) - 1,
+            currQueenPos.get(0) - 1,
+            nextQueenPos.get(1) - 1,
+            nextQueenPos.get(0) - 1,
+            arrowPos.get(1) - 1,
+            arrowPos.get(0) - 1
+        );
         gui.updateGameState(currQueenPos, nextQueenPos, arrowPos);
         if (monteCarlo != null) {
             monteCarlo.rootFromAction(action);
         }
     }
 
-    //Lays out the initial board configuration and primes the Monte Carlo engine
-    private void setupBoard() {
-        System.out.println("Board setup starting...");
+    public void InitalizeBoard() {
+        System.out.println("Initializing board");
 
         int[][][] state = new int[2][10][10];
 
-        //these hard-coded positions place the queens in their starting spots
+        // hard coded but ideally set using stateArr
         state[0][0][3] = WHITE_QUEEN;
         state[0][0][6] = WHITE_QUEEN;
+
         state[0][3][0] = WHITE_QUEEN;
         state[0][3][9] = WHITE_QUEEN;
+
         state[0][6][0] = BLACK_QUEEN;
         state[0][6][9] = BLACK_QUEEN;
+
         state[0][9][3] = BLACK_QUEEN;
         state[0][9][6] = BLACK_QUEEN;
 
